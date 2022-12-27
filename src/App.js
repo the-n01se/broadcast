5103dc836bc0f32fc14e0fb4b97d33d63f119802
Artem <the_n01se@mail.ru>
2022-12-27 10:06:58 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {spotify_icon, } from "./assets/images"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <nav className="header__menu">
+          <a href="#" className="logo__link">
+            <img src="./" alt="castaway logo" className="logo__img" />
+          </a>
+          <ul className="menu__list">
+            <li className="menu__list-item">
+              <a href="#" className="menu__list-link">Home</a>
+            </li>
+            <li className="menu__list-item">
+              <a href="#" className="menu__list-link">Episodes</a>
+            </li>
+            <li className="menu__list-item">
+              <a href="#" className="menu__list-link">About</a>
+            </li>
+            <li className="menu__list-item">
+              <a href="#" className="menu__list-link">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__content">
+          <img src="./" alt="header image" className="header__image" />
+          <div className="header__titles">
+            <h1 className="header__title">Take your podcast to the <span>next</span> <span>level</span></h1>
+          </div>
+          <div className="header__links">
+            Listen on
+            <div className="players__links">
+              <a href="#" className="players__link">
+                <img src="./" alt="" className="players__link-image" />
+              </a>
+              <a href="#" className="players__link">
+                <img src="./" alt="" className="players__link-image" />
+              </a>
+              <a href="#" className="players__link">
+                <img src="./" alt="" className="players__link-image" />
+              </a>
+              <a href="#" className="players__link">
+                <img src="./" alt="" className="players__link-image" />
+              </a>
+              <a href="#" className="players__link">
+                <img src="./" alt="" className="players__link-image" />
+              </a>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
